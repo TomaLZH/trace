76ac1acbaa8faef8bf01b421cd8381114cf71571
Justin Tey <teyjustin93@gmail.com>
2019-05-31 10:37:00 +0800

    31/5 test

--- trace/ViewController.swift
+++ trace/ViewController.swift
@@ -7,13 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var lm : CLLocationManager?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        lm = CLLocationManager()
+        lm?.delegate = self
+        lm?.desiredAccuracy = kCLLocationAccuracyBest
+        lm?.distanceFilter = 0
+        lm?.requestWhenInUseAuthorization()
+        
+        lm?.startUpdatingLocation()
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        var location = locations.last!
+    }
+    
+    
 
 
 }
